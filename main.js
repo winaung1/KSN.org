77c0275602631a994c9929044494725dbe813ed9
winaung1 <91712429+winaung1@users.noreply.github.com>
2022-03-28 15:56:02 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() { 
+    $("#bars").click(function() { 
+    $("#sideBar").toggle(); 
+    }); 
+    }); 
+
+$(document).ready(function() { 
+    $("#dropdown-menu").hover(function() { 
+    $("#dropdown-content").toggle(); 
+    }); 
+    }); 
+
+
+    
+    
+    var myIndex = 0;
+    carousel();
+    
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}    
+          x[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 6000); // Change image every 6 seconds
+        }
+          
+  
