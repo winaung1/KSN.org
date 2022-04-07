cab8a3a0a034adea56333d1f3f900ccf8efb6509
winaung1 <91712429+winaung1@users.noreply.github.com>
2022-04-07 11:51:50 -0600

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            if (scroll > 100) {
+              $(".nav").css("background" , "transparent");
+              $(".nav").css("color" , "#fff");
+            }
+            else{
+                $(".nav").css("background" , "#fff");  	
+                $(".nav").css("color" , "#000");  	
+            }
+        })
+var navbar = document.getElementById('navbar');
+var menu = document.getElementById('menu');
+var nav= document.getElementById('nav');
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop && window.matchMedia("(min-width: 640px)").matches){
+        nav.classList.add('sticky');
+    }
+    else{
+        nav.classList.remove('sticky');
+    }
+}
+
 $(document).ready(function() { 
     $("#bars").click(function() { 
     $("#sideBar").toggle(); 
@@ -9,9 +35,9 @@ $(document).ready(function() {
     $("#dropdown-content").toggle(); 
     }); 
     }); 
-
-
-    
+      })
+const trunc = document.querySelector('.trunc');
+trunc.innerHTML = trunc.innerHTML.substring(0, 150) + '...'
     
     var myIndex = 0;
     carousel();
